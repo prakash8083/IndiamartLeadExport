de424c29c1570e3a86c13cecf233195e777a882d
prakash8083 <prakash8083@gmail.com>
2021-07-30 23:19:45 +0530

    Add files via upload

---
+++ IndiaMart_CSV/LeadsExtractor.cs
@@ -0,0 +1,92 @@
+﻿using IndiaMart_CSV.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IndiaMart_CSV
+{
+    public partial class LeadsExtractor : Form
+    {
+        public LeadsExtractor()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnExport_Click(object sender, EventArgs e)
+        {
+            if(string.IsNullOrEmpty(txtAPIKey.Text))
+            {
+                MessageBox.Show("API Key is required");
+                return;
+            }
+            if (string.IsNullOrEmpty(txtMobile.Text))
+            {
+                MessageBox.Show("Registered Mobile is required");
+                return;
+            }
+
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri("http://mapi.indiamart.com/");
+
+            HttpResponseMessage msg = await client.GetAsync(string.Format("wservce/enquiry/listing/GLUSR_MOBILE/"+txtMobile.Text+"/GLUSR_MOBILE_KEY/"+txtAPIKey.Text+"/Start_Time/{0}/End_Time/{1}/", dtStart.Value.ToString("dd-MMM-yyyy"), dtEndDate.Value.ToString("dd-MMM-yyyy")));
+            string resp = await msg.Content.ReadAsStringAsync();
+            List<LeadModel> leads = JsonConvert.DeserializeObject<List<LeadModel>>(resp);
+
+            if(leads.Count==0)
+            {
+                MessageBox.Show("No Data to Export");
+                return;
+            }
+            string data = "";
+            data += "QUERY_ID,QTYPE,SENDERNAME,SENDEREMAIL,SUBJECT,DATE_RE,DATE_R,DATE_TIME_RE,MOB,COUNTRY_FLAG,QUERY_MODID,ENQ_MESSAGE,ENQ_ADDRESS,ENQ_CITY,ENQ_STATE,PRODUCT_NAME,EMAIL_ALT,MOBILE_ALT,PHONE,PHONE_ALT,TOTAL_COUNT"+Environment.NewLine;
+            foreach (var item in leads)
+            {
+                data += "\""+item.QUERY_ID + "\",\""  + item.QTYPE + "\",\""  + item.SENDERNAME + "\",\"" + item.SENDEREMAIL + "\",\""  + item.SUBJECT + "\",\""  + item.DATE_RE + "\",\""  + item.DATE_R + "\",\""  + item.DATE_TIME_RE + "\",\""  +
+                    ReplaceStringLiteral(item.MOB) + "\",\""  + item.COUNTRY_FLAG
+                    + "\",\""  + item.QUERY_MODID + "\",\""  + item.ENQ_MESSAGE + "\",\""  + item.ENQ_ADDRESS + "\",\""  + item.ENQ_CITY + "\",\""  +
+                    item.ENQ_STATE + "\",\""  + item.PRODUCT_NAME + "\",\""  + item.EMAIL_ALT + "\",\"" + ReplaceStringLiteral(item.MOBILE_ALT) + "\",\"" + ReplaceStringLiteral(item.PHONE) + "\",\""  + ReplaceStringLiteral(item.PHONE_ALT) + "\",\""  + item.TOTAL_COUNT + "\""+Environment.NewLine;
+
+            }
+
+            SaveFileDialog diag = new SaveFileDialog();
+            diag.DefaultExt = "csv";
+            if (diag.ShowDialog() == DialogResult.OK)
+            {
+                using (FileStream fParameter = new FileStream(diag.FileName, FileMode.Create, FileAccess.Write))
+                {
+                    StreamWriter m_WriterParameter = new StreamWriter(fParameter);
+                    m_WriterParameter.BaseStream.Seek(0, SeekOrigin.End);
+                    m_WriterParameter.Write(data);
+                    m_WriterParameter.Flush();
+                    m_WriterParameter.Close();
+                }
+                MessageBox.Show("File Saved, Exported "+leads.Count +" data");
+            }
+
+
+             string ReplaceStringLiteral(string req)
+            {
+                if(string.IsNullOrEmpty(req))
+                {
+                    return string.Empty;
+                }
+                return req.Replace('-', ' ');
+            }
+        }
+
+        private void lnkDoc_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Process.Start("https://help.indiamart.com/knowledge-base/lms-crm-integration/");
+        }
+    }
+}
---
+++ IndiaMart_CSV/Models/LeadModel.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndiaMart_CSV.Models
+{
+    // Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(myJsonResponse);
+    public class LeadModel
+    {
+        public string RN { get; set; }
+        public string QUERY_ID { get; set; }
+        public string QTYPE { get; set; }
+        public string SENDERNAME { get; set; }
+        public string SENDEREMAIL { get; set; }
+        public string SUBJECT { get; set; }
+        public string DATE_RE { get; set; }
+        public string DATE_R { get; set; }
+        public string DATE_TIME_RE { get; set; }
+        public string GLUSR_USR_COMPANYNAME { get; set; }
+        public string READ_STATUS { get; set; }
+        public string SENDER_GLUSR_USR_ID { get; set; }
+        public string MOB { get; set; }
+        public string COUNTRY_FLAG { get; set; }
+        public string QUERY_MODID { get; set; }
+        public string LOG_TIME { get; set; }
+        public string QUERY_MODREFID { get; set; }
+        public object DIR_QUERY_MODREF_TYPE { get; set; }
+        public object ORG_SENDER_GLUSR_ID { get; set; }
+        public string ENQ_MESSAGE { get; set; }
+        public string ENQ_ADDRESS { get; set; }
+        public string ENQ_CALL_DURATION { get; set; }
+        public string ENQ_RECEIVER_MOB { get; set; }
+        public string ENQ_CITY { get; set; }
+        public string ENQ_STATE { get; set; }
+        public string PRODUCT_NAME { get; set; }
+        public string COUNTRY_ISO { get; set; }
+        public string EMAIL_ALT { get; set; }
+        public string MOBILE_ALT { get; set; }
+        public string PHONE { get; set; }
+        public string PHONE_ALT { get; set; }
+        public object IM_MEMBER_SINCE { get; set; }
+        public string TOTAL_COUNT { get; set; }
+    }
+
+
+}
